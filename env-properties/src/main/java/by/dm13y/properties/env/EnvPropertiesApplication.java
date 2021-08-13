3b530111eea3df9098d4a3c2d8741a35d6d8968a
DmitrievichD <d.dmitrievich@itransition.com>
2021-08-13 23:22:56 +0300

    add env examples for snake_case, camelCase, cebab-case

--- env-properties/src/main/java/by/dm13y/properties/env/EnvPropertiesApplication.java
+++ env-properties/src/main/java/by/dm13y/properties/env/EnvPropertiesApplication.java
@@ -1,6 +1,8 @@
 package by.dm13y.properties.env;
 
-import by.dm13y.properties.env.properties.ResourceProperties;
+import by.dm13y.properties.env.properties.CamelCaseProperties;
+import by.dm13y.properties.env.properties.CebabCaseProperties;
+import by.dm13y.properties.env.properties.SnakeCaseProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,14 +13,18 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @RequiredArgsConstructor
 public class EnvPropertiesApplication implements CommandLineRunner {
-    private final ResourceProperties resourceProperties;
+    private final CebabCaseProperties cebabCaseProperties;
+    private final SnakeCaseProperties snakeCaseProperties;
+    private final CamelCaseProperties camelCaseProperties;
 
     public static void main(String[] args) {
         SpringApplication.run(EnvPropertiesApplication.class, args);
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        System.out.println(resourceProperties.getEnvironments());
+    public void run(String... args) {
+        System.out.println(cebabCaseProperties.getVariableCase());
+        System.out.println(camelCaseProperties.getVariableCase());
+        System.out.println(snakeCaseProperties.getVariableCase());
     }
 }
--- env-properties/src/main/java/by/dm13y/properties/env/properties/CamelCaseProperties.java
+++ env-properties/src/main/java/by/dm13y/properties/env/properties/CamelCaseProperties.java
@@ -4,9 +4,12 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+/**
+ * see ENV example in the test.env
+ */
 @Data
 @Component
-@ConfigurationProperties(prefix = "test-case.in")
-public class ResourceProperties {
-    private String environments;
+@ConfigurationProperties(prefix = "env.camel-case")
+public class CamelCaseProperties {
+    private String variableCase;
 }
